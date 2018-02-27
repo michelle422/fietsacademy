a0720233b42091150c1740dba553c504edd68ad1
michelle422 <michelle.norico@vdabcampus.be>
2018-02-27 13:18:12 +0100

    commit met one-on-one associatie

--- src/main/java/be/vdab/entities/Campus.java
+++ src/main/java/be/vdab/entities/Campus.java
@@ -9,11 +9,13 @@ import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.OrderBy;
 import javax.persistence.Table;
 
@@ -38,6 +40,9 @@ public class Campus implements Serializable {
 //	@JoinColumn(name = "campusid")
 	@OrderBy("voornaam, familienaam")
 	private Set<Docent> docenten;
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "managerid")
+	private Manager manager;
 	
 	public Campus(String naam, Adres adres) {
 		setNaam(naam);
@@ -74,6 +79,10 @@ public class Campus implements Serializable {
 	}
 	public Set<Docent> getDocenten() {
 		return Collections.unmodifiableSet(docenten);
+	}
+	
+	public Manager getManager() {
+		return manager;
 	}
 	public static boolean isNaamValid(String naam) {
 		return naam != null && !naam.trim().isEmpty();
--- src/main/webapp/WEB-INF/JSP/campussen/ingemeente.jsp
+++ src/main/webapp/WEB-INF/JSP/campussen/ingemeente.jsp
@@ -29,6 +29,9 @@
 						<dd>${telefoonNr.nummer} ${telefoonNr.opmerking}</dd>
 					</c:forEach>
 				</dl>
+				<c:if test="${not empty campus.manager}">
+					Manager: ${campus.manager.voornaam} ${campus.manager.familienaam}
+				</c:if>
 			</c:forEach>
 		</ul>
 	</c:if>
